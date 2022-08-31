8df02c61fef4c3494cde5c6e1f4cdf414e3edf27
Michiel Borkent <michielborkent@gmail.com>
2022-08-31 11:51:53 +0200

    cljs.pprint

---
+++ src/sci/configs/cljs/pprint.cljs
@@ -0,0 +1,23 @@
+(ns sci.configs.cljs.pprint
+  (:require [cljs.pprint :as pp]
+            [nbb.core :as nbb]
+            [sci.core :as sci]))
+
+(def pns (sci/create-ns 'cljs.pprint nil))
+
+(defn pprint [& args]
+  (binding [*print-fn* @sci/print-fn]
+    (apply pp/pprint args)))
+
+(defn print-table [& args]
+  (binding [*print-fn* @sci/print-fn]
+    (apply pp/print-table args)))
+
+(def pprint-namespace
+  {'pprint (sci/copy-var pprint pns)
+   'print-table (sci/copy-var print-table pns)})
+
+(defn init []
+  (nbb/register-plugin!
+   ::pprint
+   {:namespaces {'cljs.pprint pprint-namespace}}))
