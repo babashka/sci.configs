b0d7dc331a3e5817703ecb579f4476eeb84d04cb
Michiel Borkent <michielborkent@gmail.com>
2023-07-30 16:59:16 +0200

    fix passing of ns

--- src/sci/configs/funcool/promesa.cljs
+++ src/sci/configs/funcool/promesa.cljs
@@ -192,7 +192,7 @@
    'run!          (sci/copy-var p/run! pns)
    'then          (sci/copy-var p/then pns)
    'thenable?     (sci/copy-var p/thenable? pns)
-   'timeout       (sci/copy-var p/timeout)
+   'timeout       (sci/copy-var p/timeout pns)
    'with-redefs   (sci/copy-var with-redefs pns)
    'wrap          (sci/copy-var p/wrap pns)
    'doseq         (sci/copy-var doseq pns)})
