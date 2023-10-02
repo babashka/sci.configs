8448d61c0eabc3c4bb21454a91cce0e68a5b22c3
Jakub Holy <jakubholy@jakubholy.net>
2023-10-02 13:15:37 +0200

    Build static SCI playground page in GitHub pages
    Build a GitHub Pages site with an editor with pre-loaded all libs
    this sci.configs support, so that people may play with them.

---
+++ playground/src/playground.cljs
@@ -0,0 +1,171 @@
+(ns playground
+  "Build CodeMirror editor with SCI evaluation for the SCI Playground."
+  (:require
+   [sci.core :as sci]
+   [clojure.string :as str]
+
+   ;; All the configs
+   sci.configs.applied-science.js-interop
+   sci.configs.cljs.pprint
+   sci.configs.cljs.test
+   ;sci.configs.clojure.test
+   sci.configs.fulcro.fulcro
+   sci.configs.funcool.promesa
+   sci.configs.mfikes.cljs-bean
+   sci.configs.re-frame.re-frame
+   sci.configs.reagent.reagent
+   sci.configs.reagent.reagent-dom-server
+   sci.configs.tonsky.datascript
+   ; sci.configs.clojure-1-11
+   
+   ;; Code editor
+   ;; Inspiration: https://github.com/nextjournal/clojure-mode/blob/main/demo/src/nextjournal/clojure_mode/demo.cljs
+   ["@codemirror/commands" :refer [history historyKeymap]]
+   ["@codemirror/language" :refer [#_foldGutter
+                                   syntaxHighlighting
+                                   defaultHighlightStyle]]
+   ["@codemirror/state" :refer [EditorState]]
+   ["@codemirror/view" :as view :refer [EditorView lineNumbers showPanel]]
+
+  
+   ;; JS deps for re-export to sci
+   ["react" :as react]
+   ["react-dom" :as react-dom]
+
+   [nextjournal.clojure-mode :as cm-clj]
+   ))
+
+;; Necessary to avoid the error 'Attempting to call unbound fn: #'clojure.core/*print-fn*'
+;; when calling `println` inside the evaluated code
+(enable-console-print!)
+(sci/alter-var-root sci/print-fn (constantly *print-fn*))
+(sci/alter-var-root sci/print-err-fn (constantly *print-err-fn*))
+
+;; ------------------------------------------------------------ SCI eval
+
+(def all-configs ; vars so that we can extract ns info
+  [#'sci.configs.applied-science.js-interop/config
+   #'sci.configs.cljs.pprint/config
+   #'sci.configs.cljs.test/config
+   ;#'sci.configs.clojure.test/config
+   #'sci.configs.fulcro.fulcro/config
+   #'sci.configs.funcool.promesa/config
+   #'sci.configs.mfikes.cljs-bean/config
+   #'sci.configs.re-frame.re-frame/config
+   #'sci.configs.reagent.reagent/config
+   #'sci.configs.reagent.reagent-dom-server/config
+   #'sci.configs.tonsky.datascript/config])
+
+(def sci-ctx
+  (->> all-configs
+       (map deref)
+       (reduce
+        sci/merge-opts
+        (sci/init {:classes {'js js/globalThis :allow :all}
+                   :js-libs {"react" react
+                             "react-dom" react-dom}}))))
+
+(defn eval-code [code]
+  (try (sci/eval-string* sci-ctx code)
+       (catch :default e
+         (try (js/console.log "Evaluation failed:" (ex-message e)
+                              (some-> e ex-data clj->js)) 
+              (catch :default _))
+         {::error (str (.-message e)) :data (ex-data e)})))
+
+(defn eval-all [on-result  x]
+  (on-result (some->> (.-doc (.-state x)) str eval-code))
+  true)
+
+(defn sci-extension [on-result]
+  (.of view/keymap
+       #js [#js {:key "Mod-Enter" ; Cmd or Ctrl
+                 :run (partial eval-all on-result)}]))
+
+;; ------------------------------------------------------------ Code editor
+
+(defn mac? []
+  (some? (re-find #"(Mac)|(iPhone)|(iPad)|(iPod)" js/navigator.platform)))
+
+(defn output-panel-extension
+  "Display a panel below the editor with the output of the
+   last evaluation (read from the passed-in `result-atom`)"
+  [result-atom]
+  (let [dom (js/document.createElement "div")]
+    (add-watch result-atom :output-panel
+               (fn [_ _ _ new]
+                 (if (::error new)
+                   (do
+                     (.add (.-classList dom) "error")
+                     (set! (.-textContent dom) (str "ERROR: " (::error new)
+                                                    (some->> new :data pr-str (str " ")))))
+                   (do
+                     (.remove (.-classList dom) "error")
+                     (set! (.-textContent dom) (str ";; => " (pr-str new)))))))
+    (set! (.-className dom) "cm-output-panel")
+    (set! (.-textContent dom)
+          (str "Press "
+               (if (mac?) "Cmd" "Ctrl")
+               "-Enter in the editor to evaluate it. Return value will show up here."))
+    (.of showPanel
+         (fn [_view] #js {:dom dom}))))
+
+(def theme
+  (.theme
+   EditorView
+   #js {".cm-output-panel.error" #js {:color "red"}}))
+
+(defonce extensions
+  #js[theme
+      (history)
+      (syntaxHighlighting defaultHighlightStyle)
+      (view/drawSelection)
+      (lineNumbers)
+      (.. EditorState -allowMultipleSelections (of true))
+      cm-clj/default-extensions
+      (.of view/keymap cm-clj/complete-keymap)
+      (.of view/keymap historyKeymap)])
+
+(defn bind-editor! [el code]
+  {:pre [el code]}
+  (let [target-el (js/document.createElement "div")
+        last-result (atom nil)
+        exts (.concat extensions
+                      #js [(sci-extension (partial reset! last-result))
+                           (output-panel-extension last-result)])]
+    (.replaceWith el target-el)
+    (new EditorView
+         #js {:parent target-el
+              :state (.create EditorState #js {:doc code
+                                               :extensions exts})})))
+
+(defn list-libraries [all-config-vars]
+  (->> all-config-vars
+       (map (comp name :ns meta))
+       (map #(clojure.string/replace % #"^sci\.configs\.[\w-]+\." ""))
+       (remove #{"pprint" "test"})
+       sort
+       (str/join ", ")))
+
+(defn ^:export init []
+  (let [code-el (js/document.getElementById "code")
+        code (.-textContent code-el)
+        libs-el (js/document.getElementById "libs")]
+    (set! (.-textContent libs-el) (list-libraries all-configs))
+    (bind-editor! code-el code))
+  (println "Init run"))
+
+(defn ^:export reload []
+  (println "Reload run (noop)"))
+
+(comment
+  
+  (def X #'sci.configs.cljs.pprint/config)
+  
+
+  (-> X meta :ns name 
+      (clojure.string/replace-first "sci.configs." ""))
+
+
+  
+  )
